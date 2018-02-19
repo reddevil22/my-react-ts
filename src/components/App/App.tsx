619bc6798b4c08cbae98ad37d48040ffdb304c26
Joel Kenneth Fredericks <joel.fredericks@kurtosys.com>
2018-02-19 12:28:57 +0200

    Minor adjustments

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,29 +2,38 @@ import * as React from 'react';
 import './App.css';
 
 interface Props {
-	samplePropProp?: string;
+    samplePropProp?: string;
 }
 
 interface State {
-	sampleStateProp?: string;
+    sampleStateProp?: string;
 }
 
 export class App extends React.Component<Props, State> {
-	constructor(props: Props) {
-		super(props);
-	}
-	render() {
-		return (
-			<div className="App" >
-				<header className="App-header" >
-					<img src={require('../../images/logo.svg')} className="App-logo" alt="logo" />
-					<h1 className="App-title" > Welcome to React with Typescript </h1>
-					<h2 className="App-sub-title" > Kurtosys Grads </h2>
-				</header>
-				<p className="App-intro" >
-					To get started, edit <code> src / components / App / App.tsx </code> and save to reload.
-				</p>
-			</div>
-		);
-	}
+    constructor(props: Props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img
+                        src={require('../../images/logo.svg')}
+                        className="App-logo"
+                        alt="logo"
+                    />
+                    <h1 className="App-title">
+                        {' '}
+                        Welcome to React with Typescript{' '}
+                    </h1>
+                    <h2 className="App-sub-title"> Kurtosys Grads </h2>
+                </header>
+                <p className="App-intro">
+                    To get started, edit{' '}
+                    <code> src / components / App / App.tsx </code> and save to
+                    reload.
+                </p>
+            </div>
+        );
+    }
 }
